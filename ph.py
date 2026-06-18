07ae5fd0d018c0fc430ccb0e511738c667206b39
saidurgabanuru04 <saidurgabanuru04-saidurgabanuru1504@gmail.com>
2026-06-19 05:26:20 +0530

    ph.no

---
+++ ph.py
@@ -0,0 +1,10 @@
+import re
+# Read the number of test cases
+N = int(input("enter the nnumber"))
+
+for _ in range(N):
+    phone = input()
+    if re.match(r'^[7-9]\d{9}$', phone):
+        print("YES")
+    else:
+        print("NO")

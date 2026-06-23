d76347aa4b0b4ab2af9160dc9d4b132546b8395a
saidurgabanuru04 <saidurgabanuru04-saidurgabanuru1504@gmail.com>
2026-06-23 12:23:44 +0530

    list compression

--- yield.py
+++ yield.py
@@ -1,14 +1,14 @@
-raw_yields = [120, -45, 300, 0, 450, -12, 180, 600, 85]
-filtered_yields = [ round(x, 1) for x in raw_yields if x > 0 ]
+raw_yield = [120, -45, 300, 0, 450, -12, 180, 600, 85]
+filtered_yields = [ round(x, 1) for x in raw_yield if x > 0 ]
 scaled_yields = [ round (f_yield*1.15, 1) for f_yield in filtered_yields]
-analysed_yields = [ round (y*1.15, 1) for y in raw_yields if y > 0 ]
+analysed_yields = [ round (y*1.15, 1) for y in raw_yield if y > 0 ]
 
 dashboard = f"""
 ==========================================
              YIELD DASHBOARD
 ==========================================
-Raw Data Count       : {len(raw_yields)}
-Raw Yield Values     : {raw_yields}
+Raw Data Count       : {len(raw_yield)}
+Raw Yield Values     : {raw_yield}
 ------------------------------------------
 Sanitized Data Count : {len(filtered_yields)}
 Sanitized Yields     : {filtered_yields}
